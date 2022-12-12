f7494e32d74bc17ba785e9dc8373964ddd2008f5
VipiinS <Vipinshanmugam@gmail.com>
2022-12-12 20:43:37 +0530

    fetched data from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import './App.css';
-
+import React
+ from 'react';
 function App() {
+  // fro initially getting from the API
+  const [ques,setQues] = React.useState([])
+  const [quesAnswers,setQuesAndAnswers] = React.useState([]);
+
+  React.useEffect(()=>{
+    const url = "https://opentdb.com/api.php?amount=5";
+    fetch(url)
+      .then(res =>res.json())
+      .then(data => {
+        setQues(data.results)//the results contains two objects response and results,results contains our required questions and answers
+
+        //map the ques and ans into ques 
+        setQuesAndAnswers(
+          data.results.map(questionObj=>{
+            return {
+              question : questionObj.question,
+              options : [questionObj.correct_answer,
+                         questionObj.incorrect_answers],
+              correctAnswer : questionObj.correct_answer,
+              selectedAnswer : ""
+            }
+          })
+        )
+      }) 
+
+  },[])
+
   return (
     <div></div>
   );
